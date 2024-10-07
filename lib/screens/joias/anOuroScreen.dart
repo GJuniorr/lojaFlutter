cf0dd01490a52e3c84d821841f5261243bdad9b0
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:44:36 -0300

    Update anOuroScreen.dart

--- lib/screens/joias/anOuroScreen.dart
+++ lib/screens/joias/anOuroScreen.dart
@@ -78,10 +78,10 @@ class _AnOuroScreenState extends State<AnOuroScreen> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: 'kinafox223',
+        host: '',
+        port: ,
+        userName: '',
+        password: '',
       databaseName: 'apploja',
     );
     try {
