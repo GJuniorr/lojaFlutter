e00438b466e09da6bc33efa0dd4ffede5c6ff797
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:43:19 -0300

    Update listaScreen.dart

--- lib/screens/menu/listaScreen.dart
+++ lib/screens/menu/listaScreen.dart
@@ -94,10 +94,10 @@ class _listaScreenState extends State<listaScreen> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: 'kinafox223',
+        host: '',
+        port: ,
+        userName: '',
+        password: '',
       databaseName: 'apploja',
     );
     try {
