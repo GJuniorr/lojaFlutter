fddc109e52989f3fd70de04f74419667881c9c6f
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:44:14 -0300

    Update formaPagar.dart

--- lib/screens/menu/pedidos/formaPagar.dart
+++ lib/screens/menu/pedidos/formaPagar.dart
@@ -99,10 +99,10 @@ class _formaPagarState extends State<formaPagar> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: 'kinafox223',
+        host: '',
+        port: ,
+        userName: '',
+        password: '',
       databaseName: 'apploja',
     );
     try {
